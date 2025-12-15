778a370982e188e3b0374baf8eae8e7fce046f9d
jazminphilip <jazmin.philip@gmail.com>
2025-12-14 21:36:07 -0800

    Implement task management functionality in PartB.cpp

---
+++ C++/Final Part 1/PartB.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include "task_methodsPARTB.h"
+
+int main(){
+    int size = 0;
+    const int capacity = 5; //i did not do the optional part
+    int choice = 0;
+    std::string input = "";
+    int idnumber = 1;
+    int helpvar = 0;
+
+    Task* tasks = new Task[capacity]; //an array of tasks with index 0 to capacity - 1 POINTER TO HEAP
+
+do{
+    do{
+    displaymenu();
+    std::getline(std::cin, input);
+    std::cout << std::endl;
+    }
+    while(!(input == "1" || input == "2" || input == "3" || input == "4" || input == "5")); //demorgans law wow thank you
+    
+    choice = stringtoint(input);
+
+    switch(choice){
+        case 1:
+            std::cout << "Enter a description: ";
+            std::getline(std::cin, input);
+
+            helpvar = size;
+            addTask(tasks, size, capacity, input); //POINTER ALREADY MADE FROM TASKS
+            std::cout << std::endl;
+            if(helpvar == size){ //new variable to make sure that i dont affect the functions said in the assignemnt
+                break;
+            }
+
+            if(size <= capacity){ //need to make it so that it doesnt happen if the add task doesnt work
+                (tasks + size - 1) -> setId(idnumber); //needs to EXCLUSIVELY in main, not the function
+            idnumber++;
+            }
+            break;
+
+        case 2:
+            do{
+            std::cout << "Enter the number of which task to delete: ";
+            std::getline(std::cin, input);}
+            while (stringtoint(input) == 0);
+            removeTask(tasks, size, stringtoint(input));
+            break;
+
+        case 3:
+            do{
+            std::cout << "Enter the number of which task to complete: ";
+            std::getline(std::cin, input);}
+            while (stringtoint(input) == 0);
+            completeTask(tasks + stringtoint(input) - 1, stringtoint(input)); //uses part A function? was i supposed to?
+            break;                                      //tasks + input - 1 is the first, second,... element
+
+        case 4:
+            listTasks(tasks, size); 
+            break;
+        
+        case 5:
+            deletelist(tasks);
+            input = "5";
+            break;
+            
+    }
+} while (choice != 5);
+
+}
